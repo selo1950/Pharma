313007ab64ac5643129f6506bca479a46ad1e31c
selo1950 <toni.durdov7@gmail.com>
2023-04-21 11:30:13 +0200

    weight validator added

--- myapp/forms.py
+++ myapp/forms.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request
 from flask_wtf import FlaskForm
 from wtforms import StringField, RadioField, TextAreaField, SelectField, SubmitField, IntegerField,BooleanField, validators
 from .samolijecenje import medicine_clean
-from wtforms.validators import DataRequired, Optional
+from wtforms.validators import DataRequired, Optional, ValidationError
 
 indikacije = medicine_clean['Indikacija']
 indikacije_clean = set()
@@ -18,16 +18,28 @@ for clean in indikacije_clean:
 
 #print(choices_list)
 
+def validate_weight(form,field):
+    weight_entered = field.data
+    age_entered = form.age.data
 
+    if int(age_entered) < 12 and int(weight_entered) == 0:  
+        raise ValidationError('Molimo unesite težine za dijete mlađe od 12 godina!')
+    
+    return('Molimo unesite težinu za dijete mlađe od 12 godina!')
+    
 
 class Indication(FlaskForm):
     indikacija = SelectField('Odaberite vašu indikaciju: ', validate_choice=True, choices = choices_list, validators = [DataRequired()])
     age = IntegerField('Koliko imate godina?', validators=[DataRequired()])
     sex = RadioField('Spol:',  choices = [('M', 'Muško'), ('F','Žensko')], validators=[DataRequired()])
-    weight = IntegerField('Težina:', validators = [Optional()])
+    weight = IntegerField('Težina:',default = 0,  validators = [Optional(), validate_weight])
     #pregnancy = RadioField('Jeste li trudni?', choices=['Da', 'Ne'], validators=[DataRequired()])
 
     submit_ind = SubmitField('Pregledaj')
+
+
+   
+            
 
 
 
